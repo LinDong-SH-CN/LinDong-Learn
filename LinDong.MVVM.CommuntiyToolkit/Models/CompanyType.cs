5964fc2aaf594cea613c80e2de3438b72cc9e809
LinDong-SH-CN <sl13816593443@outlook.com>
2023-04-28 16:26:40 +0800

    master
    20230428

---
+++ LinDong.MVVM.CommuntiyToolkit/Models/CompanyType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinDong.MVVM.CommuntiyToolkit.Models
+{
+    public enum CompanyType
+    {
+        MainCompany = 1,
+        CustCompany = 2,
+        SuppCompany = 3,
+        MaintenanceProvider = 4,
+    }
+}
---
+++ LinDong.MVVM.CommuntiyToolkit/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LinDong.MVVM.CommuntiyToolkit.Models
+{
+    public partial class User : ObservableObject
+    {
+        [ObservableProperty]
+        private string? userId = string.Empty;
+
+        [ObservableProperty]
+        private string? name = string.Empty;
+
+        [ObservableProperty]
+        private Image? image = null;
+
+        [ObservableProperty]
+        private string? phone = string.Empty;
+
+        [ObservableProperty]
+        private string? email = string.Empty;
+
+        [ObservableProperty]
+        private string? companyId = string.Empty;
+    }
+}
