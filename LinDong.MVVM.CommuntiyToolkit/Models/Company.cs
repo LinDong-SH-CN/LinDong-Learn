222c46707ffd7f9223462fe7651b4570a0dc0103
LinDong-SH-CN <sl13816593443@outlook.com>
2023-04-28 19:24:57 +0800

    2023

--- LinDong.MVVM.CommuntiyToolkit/Models/Company.cs
+++ LinDong.MVVM.CommuntiyToolkit/Models/Company.cs
@@ -1,27 +1,3 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace LinDong.MVVM.CommuntiyToolkit.Models;
 
-namespace LinDong.MVVM.CommuntiyToolkit.Models
-{
-    public partial class Company : ObservableObject
-    {
-        [ObservableProperty]
-        private string? companyId = string.Empty;
-
-        [ObservableProperty]
-        private string? companyName = string.Empty;
-
-        [ObservableProperty]
-        private string? companyAddress = string.Empty;
-
-        [ObservableProperty]
-        private CompanyType companyType = CompanyType.MainCompany;
-
-        [ObservableProperty]
-        private string? companyDescription = string.Empty;
-    }
-}
+public record Comany(string CompanyId, string CompanyName, string CompanyAddress, CompanyType CompanyType, string CompanyDescription);
--- LinDong.MVVM.CommuntiyToolkit/Models/User.cs
+++ LinDong.MVVM.CommuntiyToolkit/Models/User.cs
@@ -1,32 +1,3 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
+﻿namespace LinDong.MVVM.CommuntiyToolkit.Models;
 
-namespace LinDong.MVVM.CommuntiyToolkit.Models
-{
-    public partial class User : ObservableObject
-    {
-        [ObservableProperty]
-        private string? userId = string.Empty;
-
-        [ObservableProperty]
-        private string? name = string.Empty;
-
-        [ObservableProperty]
-        private Image? image = null;
-
-        [ObservableProperty]
-        private string? phone = string.Empty;
-
-        [ObservableProperty]
-        private string? email = string.Empty;
-
-        [ObservableProperty]
-        private string? companyId = string.Empty;
-    }
-}
+public record User(string UserId, string Name, string Phone, string email, string ComapnyId, byte[] image);
